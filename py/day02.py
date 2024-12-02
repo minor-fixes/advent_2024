1de14893506c92c6b23086506c5bcc1fa15c3c8c
Scott Minor <minor@minorhacks.com>
2024-12-02 10:44:37 -0500

    day02: Finished part 2

--- py/day02.py
+++ py/day02.py
@@ -13,3 +13,30 @@ def part_1(f):
 
     ratings = [1 if is_safe(r) or is_safe(list(reversed(r))) else 0 for r in reports]
     return sum(ratings)
+
+
+def part_2(f):
+    reports = [[int(i) for i in line.split()] for line in f]
+
+    def removal_scan(l):
+        for i in range(len(l)):
+            yield list(l[:i] + l[i + 1 :])
+
+    def is_safe(report):
+        for i in range(len(report) - 1):
+            diff = report[i + 1] - report[i]
+            if diff < 1 or diff > 3:
+                return False
+        return True
+
+    ratings = [
+        (
+            1
+            if any(
+                is_safe(r) or is_safe(list(reversed(r))) for r in removal_scan(report)
+            )
+            else 0
+        )
+        for report in reports
+    ]
+    return sum(ratings)
